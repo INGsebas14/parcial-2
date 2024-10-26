973a35c7a39b4c1e40b0ae42365f7422defc491f
Sebastian Fonseca <fonsecasebastian86@gmail.com>
2024-10-26 08:35:34 -0500

    cambios en division

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
+//funcjion que recibe un parametro depemde del usuqrio que botn oprime
 function calculate(operation) {
+    //agarramos el valor de los imput con id(numeroUno) y lo guardamos en una variable
     let num1 = parseFloat(document.getElementById("numeroUno").value || 0);
     let num2 = parseFloat(document.getElementById("numeroDos").value || 0);
-    let resultado = document.getElementById("result");
+    let resultado ;
 
     switch (operation) {
         case "sum":
-            resultado.textContent = num1 + num2;
+            resultado = num1 + num2;
             break;
         case "res":
-            resultado.textContent = num1 - num2;
+            resultado = num1 - num2;
             break; 
         case "mul":
-            resultado.textContent = num1 * num2;
+            resultado = num1 * num2;
             break;
         case "div":
-            resultado.textContent = num1 / num2;
+            if (num2 == 0 || num1 == 0){
+                resultado = "No se puede dividir entre 0";
+            }else{
+                resultado = num1 / num2;
+            }
             break;
     }
+    document.getElementById("result").value = resultado;
 }
 
